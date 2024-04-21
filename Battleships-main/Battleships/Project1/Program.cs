3be98f13d1278b977c89ea7c0c76ddee78a796a8
Azrinek <134388105+Azrinek@users.noreply.github.com>
2024-04-21 16:08:35 +0200

    Add files via upload

---
+++ Battleships-main/Battleships/Project1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Battleship Game!");
+            Console.Write("Enter Player 1 name: ");
+            string player1Name = Console.ReadLine();
+            Console.Write("Enter Player 2 name: ");
+            string player2Name = Console.ReadLine();
+            if(player1Name == "" || player2Name == "")
+            {
+                Console.WriteLine("Player 1 and Player 2 cannot have empty names. Please try again.");
+                Main(args);
+                return;
+            }
+            if(player1Name == player2Name)
+            {
+                Console.WriteLine("Player 1 and Player 2 cannot have the same name. Please try again.");
+                Main(args);
+                return;
+            }
+            BattleshipGame game = new BattleshipGame(player1Name, player2Name);
+            game.StartGame();
+        }
+    }
+}
